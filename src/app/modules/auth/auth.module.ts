083c96047643f301860acc460b352547aa7a3326
Joel Amo <joelamodevega@hotmail.com>
2022-06-11 20:22:03 +0200

    feat(feature/login-page): Añade formulario sin funcionalidad

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    ToastModule,
   ],
   declarations: [
-    LoginComponent,
+    LoginComponent, 
     RegisterComponent,
-  ]
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [MessageService],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = this.formBuilder.group({
+    email: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+        ],
+      },
+    ],
+    password: ['', Validators.required],
+  });
 
-  constructor() { }
+  rememberMe: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {}
+
+  signIn() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    this.addMessage('error', 'Error!', 'Email o contraseñas incorrectos', 'pi pi-exlamation-triangle')
+    
   }
 
+  addMessage(severity: string, summary: string, detail: string, icon: string){
+    this.messageService.add({
+      key: 'toast',
+      severity,
+      summary,
+      detail,
+      icon,
+    })
+  }
 }
